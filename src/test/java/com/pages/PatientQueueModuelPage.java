4f1e865c1bb8fdf5495d5c60b21f48132510863c
VijayDYaligar <vijayy8981@gmail.com>
2024-03-16 18:07:12 +0530

    rectification work done as per suggetion

--- src/test/java/com/pages/PatientQueueModuelPage.java
+++ src/test/java/com/pages/PatientQueueModuelPage.java
@@ -11,5 +11,13 @@ public class PatientQueueModuelPage {
 	public void getpatientdata() {
 		System.out.println(" Doctor has received slected patient details");
 	}
+	
+	public void clickonreview() {
+		System.out.println("pages with patient details get open");
+	}
+	
+	public void rectification() {
+		System.out.println("Restification work done as per suggetion ");
+	}
 
 }
