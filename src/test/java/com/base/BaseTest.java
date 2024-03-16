03cb5a69ecfd7da80d66715dcdaa812a4192efb1
VijayDYaligar <vijayy8981@gmail.com>
2024-03-16 15:36:29 +0530

    Stablecode of project

---
+++ src/test/java/com/base/BaseTest.java
@@ -0,0 +1,9 @@
+package com.base;
+
+public class BaseTest {
+	
+	public void browserInitalize() {
+		System.out.println("Browser Get launched and application url get opened");
+	}
+
+}
---
+++ src/test/java/com/utils/BaseUtils.java
@@ -0,0 +1,8 @@
+package com.utils;
+
+public class BaseUtils {
+	public void getTestData() {
+		System.out.println("these are reusable method by which user can utilize thi sclass to get data for tst case validation in test classes ");
+	}
+
+}
