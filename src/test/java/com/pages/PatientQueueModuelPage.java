ccd811caf8eed39a5d016d17ef568c8e9c1184e5
VijayDYaligar <vijayy8981@gmail.com>
2024-03-16 17:48:01 +0530

    PatientQueueModule Work

---
+++ src/test/java/com/pages/PatientQueueModuelPage.java
@@ -0,0 +1,15 @@
+package com.pages;
+
+public class PatientQueueModuelPage {
+	public void selectDoctor() {
+		System.out.println("seleted doctor as per specielized");
+	}
+	public void enterPatientname () {
+		System.out.println(" docter has searched patient ");
+	}
+	
+	public void getpatientdata() {
+		System.out.println(" Doctor has received slected patient details");
+	}
+
+}
---
+++ src/test/java/com/tests/PatientQueueModuleTest.java
@@ -0,0 +1,12 @@
+package com.tests;
+
+public class PatientQueueModuleTest {
+	public void verifyAllusersdata() {
+		System.out.println("all users data feached by doctor as per their spetialization");
+	}
+	public void verifyDoctorGetsDetailsData() {
+		System.out.println("doctor are able to get all data of individual user");
+	}
+	
+
+}
